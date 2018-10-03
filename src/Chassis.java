df638669ed59889f55bcbbf8e868e120f3503f13
blarson1953 <larson05@gmail.com>
2018-10-03 11:54:27 -0400

    Activity 6

--- src/Chassis.java
+++ src/Chassis.java
@@ -6,6 +6,7 @@ public interface Chassis {
   //   • The definition of a public getChassisType method that returns an instance of Chassis.
   public Chassis getChassisType();
 
-  //    • The definition of a public setChassisType that accepts a string named vehicleChassis and returns a void.
+  //    • The definition of a public setChassisType that accepts a string named vehicleChassis and
+  // returns a void.
   public void setChassisType(String vehicleChassis);
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 public class Main {
 
   public static void main(String[] args) {
@@ -8,11 +9,30 @@ public class Main {
 
     VehicleChassis myVc = new VehicleChassis();
     System.out.println(myVc.toString());
-    myVc.setChassisType("Chasssis");
+    myVc.setChassisType("Chassis1111");
     System.out.println(myVc.getChassisType());
-    System.out.println(myVc.toString());
+    //System.out.println(myVc.toString());
 
-    VehicleChassis myVc2 = new VehicleChassis( "eeee");
+    VehicleChassis myVc2 = new VehicleChassis("myVc2_Chassis");
+    System.out.println(myVc2.toString());
+    myVc2.setChassisType("Chassis2222");
+    System.out.println(myVc2.getChassisType());
+    //System.out.println(myVc2.toString());
+
+    VehicleFrame myVf1 = new VehicleFrame("myVf1_frame");
+    System.out.println(myVf1.toString());
+    myVf1.setChassisType("frame11111");
+    System.out.println(myVf1.getChassisType());
+    //System.out.println(myVf1.toString());
+
+    ManufacturedEngine myEn = new ManufacturedEngine();
+    System.out.println(myEn.toString());
+    ManufacturedEngine myEn2 = new ManufacturedEngine("Honda", new Date(), "H-Series",
+        "H23A1", 4, "88 AKI", "2WD: Two-Wheel Drive");
+    System.out.println(myEn2.toString());
+
+
+
 
   }
 }
